c01dc9617446cbe12119e5adc57694cdddcf308e
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-05-16 02:10:14 +0100

    Setup structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function runGame () {
+
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+function displayMultiplyQuestion(params) {
+    
+}
